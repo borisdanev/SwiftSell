1ccfbbd84207e484496e91638dceb1f1779f5aab
borisdanev <borisdanev9@gmail.com>
2024-03-29 14:45:28 +0100

    Preloaded LCP and added _redirects file

--- src/components/StartSlider.js
+++ src/components/StartSlider.js
@@ -6,6 +6,7 @@ import Slide from "./Slide";
 import MenFashion from "../images/men_fashion.jpg";
 import FitStyle from "../images/fit_style.jpg";
 import ManWomanFashion from "../images/man_woman_fashion.jpg";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 const StartSlider = () => {
   const settings = {
     dots: true,
@@ -19,28 +20,33 @@ const StartSlider = () => {
     nextArrow: <NextArrow />,
   };
   return (
-    <section className="start">
-      <div className="container-fluid p-0">
-        <Slider
-          {...settings}
-          prevArrow={<PrevArrow />}
-          nextArrow={<NextArrow />}
-        >
-          <Slide
-            src={MenFashion}
-            text="Upgrade your closet with our premium quality fabrics and materials."
-          />
-          <Slide
-            src={FitStyle}
-            text={"Find your perfect fit with our range of sizes and styles."}
-          />
-          <Slide
-            src={ManWomanFashion}
-            text="Shop with us and get runway-inspired looks at affordable prices."
-          />
-        </Slider>
-      </div>
-    </section>
+    <HelmetProvider>
+      <section className="start">
+        <Helmet>
+          <link rel="preload" as="image" href="../images/men_fashion.jpg" />
+        </Helmet>
+        <div className="container-fluid p-0">
+          <Slider
+            {...settings}
+            prevArrow={<PrevArrow />}
+            nextArrow={<NextArrow />}
+          >
+            <Slide
+              src={MenFashion}
+              text="Upgrade your closet with our premium quality fabrics and materials."
+            />
+            <Slide
+              src={FitStyle}
+              text={"Find your perfect fit with our range of sizes and styles."}
+            />
+            <Slide
+              src={ManWomanFashion}
+              text="Shop with us and get runway-inspired looks at affordable prices."
+            />
+          </Slider>
+        </div>
+      </section>
+    </HelmetProvider>
   );
 };
 export default StartSlider;
